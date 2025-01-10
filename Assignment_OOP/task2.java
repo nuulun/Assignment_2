60dc1c99ae888c53c0e7c64caaf5b0a1aec11b97
nuulun <bekovn91@gmail.com>
2025-01-10 18:20:22 +0500

    Add files via upload

---
+++ Assignment_OOP/task2.java
@@ -0,0 +1,12 @@
+package Assignment_OOP;
+
+public class task2 {
+    public class Terran {
+    }
+
+    public class Human extends Terran {
+    }
+
+    public class Woman extends Human {
+    }
+}
---
+++ Assignment_OOP/task3.java
@@ -0,0 +1,16 @@
+package Assignment_OOP;
+
+public class task3 {
+    public class Vehicle {
+        double maxSpeed;
+    }
+
+    public class Car extends Vehicle {
+        int wheelCount;
+        double weight;
+    }
+
+    public class ElectricCar extends Car {
+        int electricEnginePower;
+    }
+}
